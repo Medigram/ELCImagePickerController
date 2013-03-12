f281d7c2f21bcacde94fbb158ab69867955c4e88
Yuhao Ding <yuhao@medigram.com>
2013-03-12 15:13:22 -0700

    Adding a spinner when picking photos

--- Classes/ELCImagePickerController.m
+++ Classes/ELCImagePickerController.m
@@ -30,28 +30,35 @@
 
 -(void)selectedAssets:(NSArray*)_assets {
 
-	NSMutableArray *returnArray = [[[NSMutableArray alloc] init] autorelease];
-	
-	for(ALAsset *asset in _assets) {
-
-		NSMutableDictionary *workingDictionary = [[NSMutableDictionary alloc] init];
-		[workingDictionary setObject:[asset valueForProperty:ALAssetPropertyType] forKey:@"UIImagePickerControllerMediaType"];
-        [workingDictionary setObject:[asset valueForProperty:ALAssetPropertyDate] forKey:@"UIImagePickerControllerMediaDate"];
-        [workingDictionary setObject:[UIImage imageWithCGImage:[[asset defaultRepresentation] fullScreenImage]] forKey:@"UIImagePickerControllerOriginalImage"];
-		[workingDictionary setObject:[[asset valueForProperty:ALAssetPropertyURLs] valueForKey:[[[asset valueForProperty:ALAssetPropertyURLs] allKeys] objectAtIndex:0]] forKey:@"UIImagePickerControllerReferenceURL"];
-		
-		[returnArray addObject:workingDictionary];
-		
-		[workingDictionary release];	
-	}
-	
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    hud.labelText = @"Importing...";
     
-    //[self popToRootViewControllerAnimated:NO];
-    //[self dismissModalViewControllerAnimated:YES];
-    
-	if([delegate respondsToSelector:@selector(elcImagePickerController:didFinishPickingMediaWithInfo:)]) {
-		[delegate performSelector:@selector(elcImagePickerController:didFinishPickingMediaWithInfo:) withObject:self withObject:[NSArray arrayWithArray:returnArray]];
-	}
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        NSMutableArray *returnArray = [[[NSMutableArray alloc] init] autorelease];
+        
+        for(ALAsset *asset in _assets) {
+            
+            NSMutableDictionary *workingDictionary = [[NSMutableDictionary alloc] init];
+            [workingDictionary setObject:[asset valueForProperty:ALAssetPropertyType] forKey:@"UIImagePickerControllerMediaType"];
+            [workingDictionary setObject:[asset valueForProperty:ALAssetPropertyDate] forKey:@"UIImagePickerControllerMediaDate"];
+            [workingDictionary setObject:[UIImage imageWithCGImage:[[asset defaultRepresentation] fullScreenImage]] forKey:@"UIImagePickerControllerOriginalImage"];
+            [workingDictionary setObject:[[asset valueForProperty:ALAssetPropertyURLs] valueForKey:[[[asset valueForProperty:ALAssetPropertyURLs] allKeys] objectAtIndex:0]] forKey:@"UIImagePickerControllerReferenceURL"];
+            
+            [returnArray addObject:workingDictionary];
+            
+            [workingDictionary release];	
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
+            
+            //[self popToRootViewControllerAnimated:NO];
+            //[self dismissModalViewControllerAnimated:YES];
+            if([delegate respondsToSelector:@selector(elcImagePickerController:didFinishPickingMediaWithInfo:)]) {
+                [delegate performSelector:@selector(elcImagePickerController:didFinishPickingMediaWithInfo:) withObject:self withObject:[NSArray arrayWithArray:returnArray]];
+            }
+        });
+    });
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
